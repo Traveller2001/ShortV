cbc0d1d729677664a7463202ff59c1ad81fbda5a
Traveller2001 <972437823@qq.com>
2025-03-08 00:52:14 +0800

    modify README.md

--- eval.sh
+++ eval.sh
@@ -8,7 +8,7 @@ MODEL_PATH=""  # Path to pretrained weights
 MODEL_NAME=""  # Identifier for output logs
 CONV_MODE=""   # Conversation template name
 
-# Architecture Configuration (Refer to technical documentation)
+# Architecture Configuration (Refer to Paper)
 export SKIP_LAYERS="31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13"
 
 accelerate launch --main_process_port 23456 --num_processes=${GPU} \
